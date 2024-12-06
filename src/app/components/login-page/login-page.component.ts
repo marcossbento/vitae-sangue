67c9c8ae805894ef5a560a51541a626261a22c8f
marcossbento <marcossbento14@gmail.com>
2024-12-06 15:46:45 -0300

    feat: login page with new styles, improved layout, and additional form elements

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -4,16 +4,26 @@ import { FloatLabelModule } from 'primeng/floatlabel';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
+
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [FloatLabelModule, InputTextModule, CommonModule, FormsModule],
+  imports: [
+    FloatLabelModule,
+    InputTextModule,
+    CommonModule,
+    FormsModule,
+    PasswordModule,
+    DividerModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
-senha: any;
-email: any;
+senha: string = '';
+email: string = '';
 
 }
