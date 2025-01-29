6eb8dea4300d4f5dc392ba255c1eb4494b45e499
zé <dudd.vitor@gmail.com>
2025-01-29 18:54:22 -0300

    fix: requisition can have multiple bolsas

--- src/app/components/requisition-form/requisition-form.component.ts
+++ src/app/components/requisition-form/requisition-form.component.ts
@@ -1,12 +1,12 @@
-import { UserAuthenticatedService } from './../../services/user-authenticated.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormGroupName, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { HemocentroService } from '../../services/hemocentro.service';
 import { RequisitionService } from '../../services/requisition.service';
+import { UserAuthenticatedService } from '../../services/user-authenticated.service';
 
 @Component({
   selector: 'app-requisition-form',
@@ -17,7 +17,8 @@ import { RequisitionService } from '../../services/requisition.service';
     ButtonModule,
     ReactiveFormsModule,
     DropdownModule,
-    InputNumberModule],
+    InputNumberModule
+  ],
 })
 export class RequisitionFormComponent implements OnInit {
   formGroup: FormGroup;
@@ -45,26 +46,50 @@ export class RequisitionFormComponent implements OnInit {
     { label: 'Detalhes da Requisição', icon: 'pi pi-info-circle', completed: false, formGroupName: 'dadosRequisicao' },
   ];
 
-  constructor(private fb: FormBuilder, private hemocentroService: HemocentroService, private requisitionService: RequisitionService, private userAuthenticatedService: UserAuthenticatedService) {
+  constructor(
+    private fb: FormBuilder,
+    private hemocentroService: HemocentroService,
+    private requisitionService: RequisitionService,
+    private userAuthenticatedService: UserAuthenticatedService
+  ) {
     this.formGroup = this.fb.group({
       dadosHemocentro: this.fb.group({
         hemocentro: ['', Validators.required]
       }),
       dadosRequisicao: this.fb.group({
-        hemocomponente: ['', Validators.required],
-        abo: ['', Validators.required],
-        rh: ['', Validators.required],
-        qtdRequirida: ['', Validators.required],
+        bolsas: this.fb.array([this.createBolsaFormGroup()])
       }),
     });
+    
   }
 
   ngOnInit(): void {
     this.carregarHemocentros();
   }
 
+  createBolsaFormGroup(): FormGroup {
+    return this.fb.group({
+      hemocomponente: ['', Validators.required],
+      abo: ['', Validators.required],
+      rh: ['', Validators.required],
+      qtdRequirida: ['', Validators.required],
+    });
+  }
+
+  get bolsas(): FormArray {
+    return this.formGroup.get('dadosRequisicao.bolsas') as FormArray;
+  }
+
+  addBolsa(): void {
+    this.bolsas.push(this.createBolsaFormGroup());
+  }
+
+  removeBolsa(index: number): void {
+    this.bolsas.removeAt(index);
+  }
+
   async carregarHemocentros() {
-    var userAuthenticated = await this.userAuthenticatedService.getUserLogado()
+    var userAuthenticated = await this.userAuthenticatedService.getUserLogado();
 
     this.hemocentroService.getHemocentrosParceiros(userAuthenticated.estabelecimento.id).subscribe({
       next: (response) => {
@@ -79,6 +104,8 @@ export class RequisitionFormComponent implements OnInit {
     this.formGroup.reset();
     this.currentStep = 0;
     this.steps.forEach(step => step.completed = false);
+    this.bolsas.clear();
+    this.addBolsa(); // Adiciona uma bolsa inicial após resetar o formulário
   }
 
   nextStep() {
@@ -101,7 +128,6 @@ export class RequisitionFormComponent implements OnInit {
   }
 
   goToStep(index: number) {
-    // Só permite navegar para steps já completados ou o próximo
     if (index < this.currentStep ||
         index === this.currentStep ||
         (index === this.currentStep + 1 && this.steps[this.currentStep].completed)) {
@@ -120,12 +146,7 @@ export class RequisitionFormComponent implements OnInit {
     const payload = {
       hemocentroId: formData.dadosHemocentro.hemocentro.id,
       situacao: "PENDENTE",
-      bolsas: [{
-        hemocomponente: formData.dadosRequisicao.hemocomponente,
-        abo: formData.dadosRequisicao.abo,
-        rh: formData.dadosRequisicao.rh,
-        qtdRequerida: formData.dadosRequisicao.qtdRequerida
-      }]
+      bolsas: formData.dadosRequisicao.bolsas
     };
 
     this.requisitionService.createRequisicao(payload).subscribe({
@@ -138,6 +159,6 @@ export class RequisitionFormComponent implements OnInit {
         console.error('Erro ao salvar requisição:', error);
         alert(`Erro: ${error.error.message || 'Falha na comunicação'}`);
       }
-    })
+    });
   }
 }
