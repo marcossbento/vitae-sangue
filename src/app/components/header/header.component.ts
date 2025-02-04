f83e133fb014f39d45139cc2e8588f653339fca0
marcossbento <marcossbento14@gmail.com>
2025-02-03 21:18:10 -0300

    feat: implement user profile dropdown with logout functionality at header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,36 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
-import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AvatarModule,
-    AvatarGroupModule,
-  ],
+  imports: [AvatarModule, AvatarGroupModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit {
-user: any = {
-  name: 'carregando...',
-  nameFirstLetter: 'C',
+export class HeaderComponent {
+  user: any = {
+    name: 'carregando...',
+    nameFirstLetter: 'C',
+  };
 
-};
+  private leaveTimer: any;
+  showDropdown = false;
 
-constructor (private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
 
+  ngOnInit() {
+    this.loadUserData();
+  }
 
-ngOnInit() {
-  this.userService.getLoggedUser().subscribe({
-    next: (userData) => {
-      this.user = userData;
-    },
-    error: () => {
-      this.user.name = 'Erro ao carregar nome';
-      this.user.email = 'Erro ao carregar email';
-    }
-  });
-}
+  private loadUserData(): void {
+    this.userService.getLoggedUser().subscribe({
+      next: (userData) => {
+        this.user = userData;
+        this.user.nameFirstLetter = userData.name.charAt(0).toUpperCase();
+      },
+      error: () => {
+        this.user.name = 'Erro ao carregar nome';
+      }
+    });
+  }
+
+  toggleDropdown(): void {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  hideDropdown(): void {
+    this.showDropdown = false;
+  }
+
+  onMouseLeave(): void {
+    this.leaveTimer = setTimeout(() => {
+      this.showDropdown = false;
+    }, 300);
+  }
+
+  cancelLeave(): void {
+    clearTimeout(this.leaveTimer);
+  }
+
+  logout(): void {
+    // Limpar token e dados do usuário
+    document.cookie = 'token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    localStorage.clear();
+    
+    this.router.navigate(['/login']);
+    
+    this.hideDropdown();
+  }
 }
